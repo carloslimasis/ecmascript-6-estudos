5d78e02357dc0078f345dbf039de7ba8d45cec5f
Carlos Lima <carloslimasis@gmail.com>
2017-06-27 17:55:16 -0300

    Add map examples

---
+++ map/exemplo1.js
@@ -0,0 +1,9 @@
+var numeros = [1,2,3];
+var dobro = [];
+
+for(var i = 0; i < numeros.length; i++) {
+    dobro.push(numeros[i] * 2);
+}
+
+console.log(numeros);
+console.log(dobro);
---
+++ map/exemplo2.js
@@ -0,0 +1,7 @@
+var numeros = [1,2,3];
+var dobro = numeros.map(function(numero){
+    return numero * 2;
+});
+
+console.log(numeros);
+console.log(dobro);
